0123a911629060f45e4ee50687a190d69ff7cdb3
Mark Li <markli@Marks-MacBook-Pro.local>
2020-02-19 23:23:55 +0800

    simple range

---
+++ automateboringstuff/05_range_steps.py
@@ -0,0 +1,9 @@
+# This will test the range parameters, steps
+
+for i in range(1, 10, 2):
+    print('Mark is printing: ' + str(i))
+
+print('....')
+
+for i in range(10, 0, -2):
+    print('Mark is printing: ' + str(i))
