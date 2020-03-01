f08489156686d3b661bca9cebb13944386138b20
markstig <41573591+markstig@users.noreply.github.com>
2020-03-01 23:04:11 +0800

    Update 08_try_except.py

--- automateboringstuff/08_try_except.py
+++ automateboringstuff/08_try_except.py
@@ -10,3 +10,18 @@ while True:
         print('Error: You tried to divided by zero')
     except ValueError:
         print('Error: You did not enter a number')
+
+# The second verson
+while True:
+    try:
+        enumerator = int(input('Please enter the enumerator: ')
+        denominator = int(input('Please enter the denominator: ')
+        result = enumerator / denominator
+        print('The result is ' + str(result))
+        continue1 = input('Continue or not? Y/N>>>> ')
+        if continue1 == 'N':
+            break
+    except ZeroDivisonError:
+        print('The denominator should not be 0')
+    except ValueError:
+        print('The input should be a number.)
