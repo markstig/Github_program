4f880951da24b6ab4464ec628dc7cde843eb3d5e
markstig <haoranana@gmail.com>
2020-03-18 21:57:55 +0800

    if name main

---
+++ other_small_projects/first_module.py
@@ -0,0 +1,8 @@
+print('First Moduel\' name is: {}'.format(__name__))
+
+# def main():
+    # pass
+
+# if __name__ == '__main__':
+    # main()
+
---
+++ other_small_projects/second_module.py
@@ -0,0 +1,3 @@
+import first_module
+
+print('The Second Module\'s name is: {}'.format(__name__))
